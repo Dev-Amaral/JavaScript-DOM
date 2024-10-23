affb4a4912966244c0d8d4aa5c8b1e2149749195
Davi Amaral <davi.amaral.estudante@gmail.com>
2024-10-23 20:02:29 -0300

    aula 23.10

--- exercicio21.10/script.js
+++ exercicio21.10/script.js
@@ -1,58 +1,84 @@
-let texto = document.querySelector('p')
-let titulo = document.querySelector('h1')
+let texto = document.querySelector("p")
+let titulo = document.querySelector("h1")
 let fundo = document.querySelector("body")
+let img = document.querySelector("img")
 
 
-let clickFundo = 1
-function corFundo(){
-if(clickFundo == 1){
+ let corFundo = ()=>{
+
+if(fundo.style.backgroundColor == "white"){
     fundo.style.backgroundColor = "black"
-    titulo.style.color = "white"
-    texto.style.color = "white"
-    clickFundo = 0
+    fundo.style.color = "white"
 } else{
     fundo.style.backgroundColor = "white"
-    titulo.style.color = "black"
-    texto.style.color = "black"
-    clickFundo = 1
+    fundo.style.color = "black"
 }
 }
 
 
-let clickTitulo =0;
-function alterarTitulo(){
-if(clickTitulo==1){
+clickTitulo= 0
+let  alterarTitulo = () => {
+    let titulo = document.querySelector("h1")
+
+if(clickTitulo == 1){
     titulo.innerText = "Que sonho! Neymar brilha, Barcelona bate Juve e é penta da Champions"
-    clickTitulo = 0
-}
-else{
+    clickTitulo =0
+}else{
+    clickTitulo=1
 titulo.innerText = "Gol de Neymar, decretando o título do Barcelona sobre a Juventus é o 27º mais marcante da Era Moderna da Champions"
-clickTitulo = 1
 }
 
 }
 
 
-let clickCorTexto = 1;
-function corTexto(){
-    if(clickCorTexto ==1){
-        texto.style.color = "yellow"
-        clickCorTexto = 0
+let corTexto = () =>{
+    let texto = document.querySelector("p")
+    let titulo = document.querySelector("h1")
+    if(texto.style.color == 'black'){
+        texto.style.color = 'blue'
+        titulo.style.color = 'blue'
     }else{
-        texto.style.color = "black"
-        clickCorTexto = 1
+        texto.style.color = 'black'
+        titulo.style.color = 'black'
     }
 }
 
 
-let clickfonte=0
+
 function fonte(){
-if(clickfonte==1){
+if(texto.style.fontSize === '15px'){
     texto.style.fontSize = '20px'
-    clickfonte = 0
 } else{
-    texto.style.fontSize = '10px'
-    clickfonte =1
+    texto.style.fontSize = '15px'
 }
 }
+
+function shadow(){
+if(texto.style.textShadow = '2px 2px 10px rgba(0,0,0,0.5)') {
+    texto.style.textShadow = ''
+}   else{
+    texto.style.textShadow = '2px 2px 10px rgba(0,0,0,0.5)'
+}
+}
+
+
+function corParagrafo(){
+    let texto = document.querySelector("p")
+    if(texto.style.color == 'black'){
+        texto.style.color = 'blue'
+    } else{
+        texto.style.color  = 'black'
+    }
+}
+
+
+function imagem(){
+let img = document.querySelector("img")
+if(img.style.display =="none"){
+    img.style.display = "flex"
+}else{
+    img.style.display= "none"
+}
+}
+
 
